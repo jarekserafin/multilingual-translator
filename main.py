30dda43d07044797249a056c3e34e070824a1f09
jarekserafin <jarek.serafin@gmail.com>
2022-12-05 22:08:34 +0100

    defined functions from reusable pieces of code

--- main.py
+++ main.py
@@ -1,60 +1,65 @@
 import requests
-
 from bs4 import BeautifulSoup
+import time
+
+
+def take_input():
+    print(msg_home)
+    home_language = int(input())
+    print(msg_foreign)
+    foreign_language = int(input())
+    print(msg_word)
+    word = input().lower()
+    home_language = languages[home_language - 1]
+    foreign_language = languages[foreign_language - 1]
+    return home_language, foreign_language, word
+
+
+def url_maker(lan_1, lan_2):
+    return "https://context.reverso.net/translation/" + lan_1.lower() + "-" + lan_2.lower() + "/" + word
+
+
+def get_content(url):
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    spans = soup.find_all('span', {'class': "display-term"})
+    new_translations = list()
+    for x in spans:
+        new_translations.append(x.text)
+    divs_examples = soup.find_all('div', {'class': ['src ltr', 'trg ltr']})
+    for x in divs_examples:
+        examples.append(x.text)
+    new_examples = [item.strip() for item in examples]
+    return new_translations, new_examples
+
 
 # headers needed to get 200 acceptance from website instead of 403
 headers = {'User-Agent': 'Mozilla/5.0'}
-
+translations = list()
+examples = list()
 msg_welcome = "Hello, welcome to the translator. Translator supports: "
-languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch', 'Polish',
-             'Portuguese', 'Romanian', 'Russian', 'Turkish']
-for ind, language in enumerate(languages, start=1):
-    print(str(ind) + ".", language)
-
 msg_home = "Type the number of your language: "
 msg_foreign = "Type the number of language you want to translate to: "
 msg_word = "Type the word you want to translate:"
+languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch', 'Polish',
+             'Portuguese', 'Romanian', 'Russian', 'Turkish']
 
-print(msg_home)
-home_language = int(input())
-print(msg_foreign)
-foreign_language = int(input())
-print(msg_word)
-word = input().lower()
+print(msg_welcome)
+for ind, language in enumerate(languages, start=1):
+    print(str(ind) + ".", language)
 
-home_language = languages[home_language - 1]
-foreign_language = languages[foreign_language - 1]
+home_language, foreign_language, word = take_input()
+url = url_maker(home_language, foreign_language)
 
-# valid url to get data from
-url = "https://context.reverso.net/translation/" + home_language.lower() + "-" + foreign_language.lower() + "/" + word
-
-# using request module to connect with url
-r = requests.get(url, headers=headers)
 print(foreign_language + " Translations:")
-
-# using BeautifulSoup module to get content from website
-soup = BeautifulSoup(r.content, 'html.parser')
-translations = list()
-sentences = list()
-
-# storing all span with the given class in variable spans
-spans = soup.find_all('span', {'class': "display-term"})
-
-# appending translations into list
-for x in spans:
-    translations.append(x.text)
-
+print()
+translations, examples = get_content(url)
 for word in translations:
     print(word)
 
-# appending content sentences into list
-divs_examples = soup.find_all('div', {'class': ['src ltr', 'trg ltr']})
-for x in divs_examples:
-    sentences.append(x.text)
-
-new_sentences = [item.strip() for item in sentences]
 print()
-
 print(foreign_language + " Examples:")
-for example in new_sentences:
+for i, example in enumerate(examples):
+    if i % 2 == 0:
+        print()
     print(example)
