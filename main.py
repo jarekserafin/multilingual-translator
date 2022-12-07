08650f9b0a73b8e40c3e7e02b01b635f2e42f9b7
jarekserafin <jarek.serafin@gmail.com>
2022-12-07 20:31:41 +0100

    added translating to all languages simultaneously

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def take_input():
     print(msg_word)
     word = input().lower()
     home_language = languages[home_language - 1]
-    foreign_language = languages[foreign_language - 1]
+    if foreign_language != 0:
+        foreign_language = languages[foreign_language - 1]
     return home_language, foreign_language, word
 
 
@@ -19,7 +20,7 @@ def url_maker(lan_1, lan_2):
     return "https://context.reverso.net/translation/" + lan_1.lower() + "-" + lan_2.lower() + "/" + word
 
 
-def get_content(url):
+def get_translations(url):
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
     spans = soup.find_all('span', {'class': "display-term"})
@@ -33,33 +34,67 @@ def get_content(url):
     return new_translations, new_examples
 
 
+def get_translations_for_all(url):
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    spans = soup.find('span', {'class': "display-term"})
+    new_translations = list()
+    examples = list()
+    for x in spans:
+        new_translations.append(x.text)
+    divs_examples = soup.find_all('div', {'class': ['src ltr', 'trg ltr']})
+    for i, x in enumerate(divs_examples):
+        examples.append(x.text)
+        if i == 1:
+            break
+    new_examples = [item.strip() for item in examples]
+    return new_translations, new_examples
+
 # headers needed to get 200 acceptance from website instead of 403
 headers = {'User-Agent': 'Mozilla/5.0'}
 translations = list()
 examples = list()
 msg_welcome = "Hello, welcome to the translator. Translator supports: "
 msg_home = "Type the number of your language: "
-msg_foreign = "Type the number of language you want to translate to: "
+msg_foreign = "Type the number of a language you want to translate to or '0' to translate to all languages: "
 msg_word = "Type the word you want to translate:"
 languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch', 'Polish',
              'Portuguese', 'Romanian', 'Russian', 'Turkish']
+
+
 
 print(msg_welcome)
 for ind, language in enumerate(languages, start=1):
     print(str(ind) + ".", language)
 
 home_language, foreign_language, word = take_input()
-url = url_maker(home_language, foreign_language)
 
-print(foreign_language + " Translations:")
-print()
-translations, examples = get_content(url)
-for word in translations:
-    print(word)
+if foreign_language != 0:
+    url = url_maker(home_language, foreign_language)
+    translations, examples = get_translations(url)
 
-print()
-print(foreign_language + " Examples:")
-for i, example in enumerate(examples):
-    if i % 2 == 0:
+    print(foreign_language + " Translations:")
+    print()
+    for word in translations:
+        print(word)
+    print()
+    print(foreign_language + " Examples:")
+    for i, example in enumerate(examples):
+        if i % 2 == 0:
+            print()
+        print(example)
+
+else:
+    for ind, foreign_language in enumerate(languages, start=1):
+        if foreign_language == home_language:
+            continue
+        url = url_maker(home_language, foreign_language)
+
+        translation, examples = get_translations_for_all(url)
+        print(foreign_language + " Translations:")
+        print(translation[0])
         print()
-    print(example)
+        print(foreign_language + " Examples:")
+        for example in examples:
+            print(example)
+        print()
